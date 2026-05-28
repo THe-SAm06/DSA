6674d32626b1225a111c44860bd127c5439499d4
THe-SAm06 <thesameer.patel2006@gmail.com>
2026-05-28 15:36:33 +0530

    Time: 1252 ms (5.02%) | Memory: 9.4 MB (66.68%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool ispal(string& s,int i, int j){
+        while(i<=j){
+            if(s[i]!=s[j]) return false;
+            i++, j--;
+        }
+        return true;
+    }
+    string longestPalindrome(string s) {
+        int maxlen = 0;
+        int start;
+        for(int i=0;i<s.size();i++){
+            for(int j =0;j<s.size();j++){
+                if(ispal(s,i,j) && maxlen<j-i+1){
+                    start = i;
+                    maxlen = j-i+1;
+                }
+            }
+        }
+        return s.substr(start,maxlen);
+    }
+};
