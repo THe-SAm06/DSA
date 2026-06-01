52b37625f8259384d97ce9b3590c829cece9dc26
THe-SAm06 <thesameer.patel2006@gmail.com>
2026-06-01 15:15:37 +0530

    Time: 2 ms (92.71%) | Memory: 23.8 MB (81.65%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -6,9 +6,8 @@ public:
         int r = intervals[0][1];
         vector<vector<int>> ans;
         for(int i = 1;i<intervals.size();i++){
-            if((intervals[i][0]<=l && intervals[i][1]<=r && intervals[i][1]>=l) || (intervals[i][1]>=r && intervals[i][0]>=l && intervals[i][0]<=r) || (intervals[i][0]<=l && intervals[i][1]>=r) || (intervals[i][0]>=l && intervals[i][1]<=r) ){
-                l = min(min(l,r),min(intervals[i][0],intervals[i][1]));
-                r = max(max(l,r),max(intervals[i][0],intervals[i][1]));
+            if(intervals[i][0]<=r){
+                r = max(r,intervals[i][1]);
             }
             else{
                 ans.push_back({l,r});
