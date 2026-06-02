78d7053f27dba8cdb660122a47003227f8ae5ea2
THe-SAm06 <thesameer.patel2006@gmail.com>
2026-06-02 22:32:56 +0530

    Time: 20 ms (15.91%) | Memory: 33.2 MB (5.05%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -10,30 +10,43 @@
  */
 class Solution {
 public:
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
+    ListNode* merge(ListNode* l1,ListNode* l2){
+        ListNode* lists1 = l1;
+        ListNode* lists2 = l2;
         ListNode* dm = new ListNode(-1);
         ListNode* temp = dm;
-        int count =0;
-        for(int i=0;i<lists.size();i++){
-            ListNode* temp = lists[i];
-            while(temp){
-                count++;
-                temp = temp->next;
+        while(lists1 && lists2){
+            if(lists1->val<=lists2->val){
+                temp->next = new ListNode(lists1->val);
+                lists1 = lists1->next;
             }
+            else{
+                temp->next = new ListNode(lists2->val);
+                lists2 = lists2->next;
+            }
+            temp = temp->next;
         }
-        for(int i=0;i<count;i++){
-            int mn = INT_MAX;
-            int idx;
-            for(int j = 0;j<lists.size();j++){
-                if(lists[j] && (lists[j]->val)<=mn){
-                    mn = lists[j]->val;
-                    idx = j;
-                }
-            }
-            temp->next = new ListNode(mn);
-            lists[idx] = lists[idx]->next;
+        while(lists1){
+            temp->next = new ListNode(lists1->val);
+            lists1 = lists1->next;
+            temp = temp->next;
+        }
+        while(lists2){
+            temp->next = new ListNode(lists2->val);
+            lists2 = lists2->next;
             temp = temp->next;
         }
         return dm->next;
     }
+    ListNode* mergeKListsf(vector<ListNode*>& lists,int l, int r){
+        if(l == r) return lists[l];
+        int mid = (l+r)/2;
+        ListNode *left = mergeKListsf(lists,l,mid);
+        ListNode* right = mergeKListsf(lists,mid+1,r);
+        return merge(left,right);
+    }
+   ListNode* mergeKLists(vector<ListNode*>& lists) {
+    if(lists.empty()) return nullptr;
+    return mergeKListsf(lists,0,lists.size()-1);
+}
 };
