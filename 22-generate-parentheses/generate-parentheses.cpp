aa5db162667f8a3d668af1bff9fe0a64fdc75949
THe-SAm06 <thesameer.patel2006@gmail.com>
2026-06-07 19:15:12 +0530

    Time: 2 ms (81.52%) | Memory: 15.7 MB (51.10%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
- vector<string> v;
-        string s;
-        void back(int open,int close,int n){
-            if(open==n && close==n){
-                v.push_back(s);
-                return;
-            }
-            if(open<n){
-                s.push_back('(');
-                back(open+1,close,n);
-                s.pop_back();
-            }
-            if(close<open){
-                s.push_back(')');
-                back(open,close+1,n);
-                s.pop_back();
-            }
+    void recur(int n, vector<string>& ans,string s,int open,int close){
+        if(open == n && close == n){
+            ans.push_back(s);
+            return;
         }
+        if(close>open) return ;
+        if(open<n) recur(n,ans,s+'(',open+1,close);
+        if(close<open) recur(n,ans,s+')',open,close+1);
+    }
     vector<string> generateParenthesis(int n) {
-       back(0,0,n);
-       return v;
+        vector<string> ans;
+        recur(n,ans,"",0,0);
+        return ans;
     }
 };
