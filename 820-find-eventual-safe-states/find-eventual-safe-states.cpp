6c73f0c6994fa6ddd5bef1708c33422e4e4dcc56
THe-SAm06 <thesameer.patel2006@gmail.com>
2026-07-07 15:34:46 +0530

    Time: 3 ms (94.15%) | Memory: 51.2 MB (99.59%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,43 +1,37 @@
 class Solution {
 public:
-    void dfs(int node,vector<vector<int>>& graph,vector<bool>& vis,stack<int>& st){
+    bool dfs(int node, vector<vector<int>>& adj,vector<bool>& vis,vector<bool>& pathvis){
+        
+        if(vis[node]) return true;
+        
         vis[node] = true;
-
-        for(int v : graph[node]){
-            if(!vis[v]) dfs(v,graph,vis,st);
+        pathvis[node] = true;
+        
+        for(int v:adj[node]){
+            if(!vis[v]){
+                if(dfs(v,adj,vis,pathvis)) return true;
+            }
+            if(pathvis[v]) return true;
         }
-
-        st.push(node);
+        
+        pathvis[node] = false;
+        
+        return false;
+        
     }
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        vector<int> topsort;
-        vector<bool> vis(graph.size(),false);
-        stack<int> st;
-        for(int i=0;i<graph.size();i++){
-            if(!vis[i]) dfs(i,graph,vis,st);
-        }
-        while(!st.empty()){
-            topsort.push_back(st.top());
-            st.pop();
-        }
-        for(int a:topsort) cout<<a<<" ";
-        unordered_set<int> safe_node;
-        for(int i = topsort.size()-1;i>=0;i--){
-            if(graph[topsort[i]].empty()){
-                safe_node.insert(topsort[i]);
-                continue;
+        vector<bool> vis(graph.size());
+        vector<bool> pathvis(graph.size());
+        
+        for(int i = 0;i<graph.size();i++){
+            if(!vis[i]){
+                dfs(i,graph,vis,pathvis);
             }
-            bool flag = true;
-            for(int u:graph[topsort[i]]){
-                if(safe_node.find(u)==safe_node.end()){
-                    flag = false;
-                    break;
-                }
-            }
-            if(flag) safe_node.insert(topsort[i]);
         }
-        vector<int> ans(safe_node.begin(),safe_node.end());
-        sort(ans.begin(),ans.end());
+        vector<int> ans;
+        for(int i =0;i<vis.size();i++){
+            if(!pathvis[i]) ans.push_back(i);
+        }
         return ans;
     }
 };
