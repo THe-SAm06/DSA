f5432fd29b3e3f12882b3f22d96eebd0283a166e
THe-SAm06 <thesameer.patel2006@gmail.com>
2026-08-11 18:54:13 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (74.94%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -4,27 +4,30 @@ public:
         int n = nums.size();
 
         if(n == 1) return nums[0];
-        vector<vector<int>> dp(n,vector<int>(2,-1));
 
-        dp[0][0] = 0;
-        dp[0][1] = nums[0];
+        int not_take = 0;
+        int take = nums[0];
+
+        int p1;
 
         for(int i = 1;i<n-1;i++){
-            dp[i][0] = max(dp[i-1][0],dp[i-1][1]);
-            dp[i][1] = nums[i] + dp[i-1][0];
+            p1 = not_take;
+            not_take = max(not_take,take);
+            take = nums[i] + p1;
         }
 
-        int m1 =max(dp[n-2][0],dp[n-2][1]);
+        int m1 =max(take,not_take);
 
-        dp[1][0] = 0;
-        dp[1][1] = nums[1];
+        not_take = 0;
+        take = nums[1];
 
         for(int i = 2;i<n;i++){
-            dp[i][0] = max(dp[i-1][0],dp[i-1][1]);
-            dp[i][1] = nums[i] + dp[i-1][0];
+            p1 = not_take;
+            not_take = max(not_take,take);
+            take = nums[i] + p1;
         }
 
-        int m2 = max(dp[n-1][0],dp[n-1][1]);
+        int m2 = max(not_take,take);
 
         return max(m1,m2);
     }
