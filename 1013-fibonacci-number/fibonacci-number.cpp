25b2f10c7f3d9ed46a676c1a31a90997c62bc1a0
THe-SAm06 <thesameer.patel2006@gmail.com>
2026-08-11 11:51:42 +0530

    Time: 2 ms (77.77%) | Memory: 7.6 MB (89.11%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,13 +1,14 @@
 class Solution {
-    int fun(int n,int dp[]){
-        if(n<=1) return n;
-        if(dp[n] != -1) return dp[n];
-        return dp[n] = fun(n-1,dp) + fun(n-2,dp);
-    }
 public:
     int fib(int n) {
+        if(n<=1) return n;
         int dp[n+1];
         memset(dp,-1,sizeof(dp));
-        return fun(n,dp);
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i = 2;i<n+1;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 };
